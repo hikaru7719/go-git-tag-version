da7775060f8302a324daba15862998fd898eecb1
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2020-12-12 12:30:29 +0900

    dry-runモードの実装

--- git/command.go
+++ git/command.go
@@ -7,6 +7,9 @@ import (
 
 // New is factory method for IGit.
 func New(dryRun bool) IGit {
+	if dryRun {
+		return ImitationGit{}
+	}
 	return Git{}
 }
 
@@ -37,6 +40,10 @@ func tagVersioning(version string) (string, error) {
 	return executeGit("tag", version)
 }
 
+func tag() (string, error) {
+	return executeGit("tag")
+}
+
 func deleteTag(version string) (string, error) {
 	return executeGit("tag", "-d", version)
 }
@@ -60,7 +67,7 @@ func (g Git) TagVersioning(version string) (string, error) {
 
 // Tag executes git tag command and read all version.
 func (g Git) Tag() (string, error) {
-	return executeGit("tag")
+	return tag()
 }
 
 // DeleteTag deletes target version tag.
@@ -71,4 +78,35 @@ func (g Git) DeleteTag(version string) (string, error) {
 // PushTag executes git push command and sync remote version and local version.
 func (g Git) PushTag(version string) (string, error) {
 	return pushTag(version)
+}
+
+// ImitationGit doesn't making changes.
+type ImitationGit struct{}
+
+// Fetch executes same with Git.
+func (g ImitationGit) Fetch() (string, error) {
+	return fetch()
+}
+
+// TagVersioning do nothing.
+func (g ImitationGit) TagVersioning(version string) (string, error) {
+	log.Printf("dry-run TagVersioning method %s", version)
+	return "", nil
+}
+
+// Tag executes same with Git.
+func (g ImitationGit) Tag() (string, error) {
+	return tag()
+}
+
+// DeleteTag do nothing.
+func (g ImitationGit) DeleteTag(version string) (string, error) {
+	log.Printf("dry-run DeleteTag method %s", version)
+	return "", nil
+}
+
+// PushTag do nothing.
+func (g ImitationGit) PushTag(version string) (string, error) {
+	log.Printf("dry-run PushTag method %s", version)
+	return "", nil
 }
