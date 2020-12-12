64f2305bfe3a6cfc691ea14296a10c242c9a41c7
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2020-12-12 20:02:20 +0900

    main関数のインデントを調整

--- main.go
+++ main.go
@@ -9,13 +9,16 @@ import (
 )
 
 var (
-	major, minor, patch bool
-	dryRun              bool
-	cmd                 = &cobra.Command{
+	major  bool
+	minor  bool
+	patch  bool
+	dryRun bool
+	cmd    = &cobra.Command{
 		Use:   "go-git-tag-version",
 		Short: "go-git-tag-version suports versioning of git tag.",
 		Long: `You can use git-tag-version with --major, --minor, --patch flags like yarn version. 
 git-tag-version increments your git tag version depending on your flags.`,
+		Args: cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return runner.New(dryRun).Run(major, minor, patch)
 		},
